e6d81a9176db43b5c6b2ad1c125873d195ad71fa
IKedmenec <ivan.kec86@gmail.com>
2024-05-02 18:30:03 +0200

    promjena22

--- zadatak.js
+++ zadatak.js
@@ -2,18 +2,36 @@
 var _ = require('lodash');
 
 // 1. 
-var dobro = "a1234567890";
+// var dobro = "1234567890";
+var dobro = "123456789012324";
 var lose = "1123456";
 var jedinstevniZnakovi = function (password) {
     // vas kod
+
+    var mojNiz = [];
+console.log("password.length=" + password.length);
     for (let i = 0; i <= password.length; i++) {
         var numToCheck = password.charAt(i);
         //console.log (numToCheck + " is integer = " + isBroj(numToCheck));
+        
         if (isBroj(numToCheck) == false) {
             console.log(numToCheck + " nije broj");
-            break;
+            // break;
+            return false;
+        }
+
+        if (mojNiz.includes(numToCheck)) {
+            console.log(numToCheck + " vec postoji u nasem nizu");
+            // break;
+            return false;
+        } 
+        
+        else {
+            console.log(numToCheck + " Ne postoji u nasem nizu");
+            mojNiz.push(numToCheck);
         }
     }
+    return true;
 };
 
 function isBroj(broj) {
@@ -26,5 +44,32 @@ function isBroj(broj) {
     return false; */
 }
 
-jedinstevniZnakovi(dobro); 
+function isMoreThan10(password){
+    if(password.length > 10) {
+        return true;
+    }
+    return false;
+}
+
+function skratiNa10(password){
+    var tmpPassword = "";
+    for (let i = 0; i < 10; i++) {
+        var charAtIndex = password.charAt(i);
+        tmpPassword += charAtIndex;
+    }
+   
+    return tmpPassword;
+
+}
+
+console.log("Password je ispravan: " + jedinstevniZnakovi(dobro));
+//jedinstevniZnakovi(dobro); 
+console.log("--------");
 jedinstevniZnakovi(lose);
+console.log("Password je ispravan: " + jedinstevniZnakovi(lose));
+
+console.log("Password ima vise od 10 znakova: " + isMoreThan10(dobro));
+console.log("Password ima vise od 10 znakova: " + isMoreThan10(lose));
+
+console.log("Password nakon skracivanja: " + skratiNa10(dobro));
+console.log("Password nakon skracivanja: " + skratiNa10(lose));
